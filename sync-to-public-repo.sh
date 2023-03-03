a07499daf74db5c68aa637533b5371023dbca20d
Di Mei <di.mei@coinbase.com>
2023-03-02 14:30:35 -1000

    add script to sync

--- sync-to-public-repo.sh
+++ sync-to-public-repo.sh
@@ -41,41 +41,10 @@ if ! grep -rl 'github.cbhq.net/cloud' ./protos | xargs sed -i "" -e 's/github.cb
   exit 1
 fi
 
-#
-#if git checkout --quiet -b sync-$BRANCH_TO_SYNC right/$BRANCH_TO_SYNC; then
-#	echo "Merge branches from left and right if necessary."
-#	if ! git merge -m "Merge to sync between $INTERNAL_REPO_URL and $PUBLIC_REPO_URL" --log left/$BRANCH_TO_SYNC; then
-#                >&2 echo "Merge conflict. Solve is manually, commit and rerun script."
-#                exit 1
-#    fi
-#else
-#	echo "Rerun after merge conflict resolution or restored connection."
-#	git checkout --quiet sync-$BRANCH_TO_SYNC
-#	echo "Try to merge with right first"
-#	if ! git merge -m "Merge to sync between $INTERNAL_REPO_URL and $PUBLIC_REPO_URL" --log right/$BRANCH_TO_SYNC; then
-#                >&2 echo "Merge conflict. Solve is manually, commit and rerun script."
-#                exit 1
-#    fi
-#	if ! git merge -m "Merge to sync between $INTERNAL_REPO_URL and $PUBLIC_REPO_URL" --log left/$BRANCH_TO_SYNC; then
-#                >&2 echo "Merge conflict. Solve is manually, commit and rerun script."
-#                exit 1
-#    fi
-#fi
-#
-#echo "Push merged changes in $BRANCH_TO_SYNC to $PUBLIC_REPO_URL"
-#if ! git push $PUBLIC_REPO_URL HEAD:$BRANCH_TO_SYNC; then
-#	>&2 echo "Fatal: unable to push to $PUBLIC_REPO_URL, rerun the script as soon as connection restored."
-#	exit 1
-#
-#fi
-#echo "Push merged changes in $BRANCH_TO_SYNC to $INTERNAL_REPO_URL"
-#if ! git push $INTERNAL_REPO_URL HEAD:$BRANCH_TO_SYNC; then
-#	>&2 echo "Fatal: unable to push to $INTERNAL_REPO_URL, rerun the script as soon as connection restored."
-#	exit 1
-#
-#fi
-#
-#git checkout --quiet master
-#git branch -D --quiet sync-$BRANCH_TO_SYNC
+echo "Generate protos"
+if ! make gen; then
+  echo >&2 "Fatal: unable to generate protos"
+  exit 1
+fi
 
 echo "Done."
