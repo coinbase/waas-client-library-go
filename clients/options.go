4ae0fb7440c60826fb4cec935d59ee57b60124c5
Yuga Cohler <yuga.cohler@coinbase.com>
2023-05-25 11:12:20 -0400

    Release v1.0.1

---
+++ clients/options.go
@@ -0,0 +1,153 @@
+package clients
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+
+	"github.com/coinbase/waas-client-library-go/auth"
+	"github.com/googleapis/gax-go/v2"
+	"google.golang.org/api/option"
+)
+
+// WaaSClientConfig stores configuration information about a WaaS client.
+type WaaSClientConfig struct {
+	Endpoint      string
+	ServiceName   string
+	APIKey        *auth.APIKey
+	HTTPClient    *http.Client
+	ClientOptions []option.ClientOption
+}
+
+// WaaSClientOption is a function that applies changes to a clientConfig.
+type WaaSClientOption func(c *WaaSClientConfig)
+
+// WithEndpoint returns an option to set the service endpoint.
+func WithEndpoint(endpoint string) WaaSClientOption {
+	return func(c *WaaSClientConfig) {
+		c.Endpoint = endpoint
+	}
+}
+
+// WithAPIKey returns an option to set the Coinbase Cloud API Key.
+func WithAPIKey(apiKey *auth.APIKey) WaaSClientOption {
+	return func(c *WaaSClientConfig) {
+		c.APIKey = apiKey
+	}
+}
+
+// WithHTTPClient returns an option to set the HTTP Client.
+func WithHTTPClient(httpClient *http.Client) WaaSClientOption {
+	return func(c *WaaSClientConfig) {
+		c.HTTPClient = httpClient
+	}
+}
+
+// WithClientOptions returns an option to set the inner set of ClientOptions.
+func WithClientOptions(clientOptions ...option.ClientOption) WaaSClientOption {
+	return func(c *WaaSClientConfig) {
+		c.ClientOptions = clientOptions
+	}
+}
+
+// GetConfig returns a WaaSClientConfig with all of the WaaSClientOptions applied.
+// It uses the given default endpoint if none of the options set it.
+func GetConfig(
+	serviceName string,
+	defaultEndpoint string,
+	waasOpts ...WaaSClientOption) (*WaaSClientConfig, error) {
+	config := &WaaSClientConfig{}
+
+	for _, waasOpt := range waasOpts {
+		waasOpt(config)
+	}
+
+	if config.Endpoint == "" {
+		config.Endpoint = defaultEndpoint
+	}
+
+	if serviceName == "" {
+		return nil, errors.New("service name must be provided")
+	}
+
+	config.ServiceName = serviceName
+
+	return config, nil
+}
+
+// GetClientOptions returns the list of ClientOptions based on the given endpoint, service name,
+// and config.
+func GetClientOptions(config *WaaSClientConfig) ([]option.ClientOption, error) {
+	clientOptions := []option.ClientOption{}
+
+	// Start with explicitly indicated client options.
+	if len(config.ClientOptions) > 0 {
+		clientOptions = append(clientOptions, config.ClientOptions...)
+	}
+
+	if config.Endpoint == "" {
+		return nil, errors.New("endpoint must be provided")
+	}
+
+	clientOptions = append(clientOptions, option.WithEndpoint(config.Endpoint))
+
+	if config.ServiceName == "" {
+		return nil, errors.New("service name must be provided")
+	}
+
+	httpClient, err := GetHTTPClient(config.ServiceName, config)
+	if err != nil {
+		return nil, err
+	}
+
+	clientOptions = append(clientOptions, option.WithHTTPClient(httpClient))
+
+	return clientOptions, nil
+}
+
+// GetHTTPClient returns the HTTPClient based on the given service name and config.
+func GetHTTPClient(serviceName string, config *WaaSClientConfig) (*http.Client, error) {
+	var httpClient *http.Client
+	if config.HTTPClient != nil {
+		httpClient = config.HTTPClient
+	} else {
+		httpClient = &http.Client{}
+	}
+
+	if config.APIKey != nil {
+		if httpClient.Transport == nil {
+			httpClient.Transport = http.DefaultTransport
+		}
+
+		transport, err := NewTransport(
+			httpClient.Transport,
+			serviceName,
+			config.APIKey,
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		httpClient.Transport = transport
+	}
+
+	return httpClient, nil
+}
+
+// LongRunningOperation is the interface for long-running operations that is
+// used to create the gax call options for interacting with LROs.
+type LongRunningOperation interface {
+	Name() string
+	PathPrefix() string
+}
+
+// LROOptions returns the call options for long-running operations.
+// This overrides the gapic generated client `WithPath` call option that ignores the
+// path prefix, with a call option that includes the path prefix.
+func LROOptions(op LongRunningOperation, version string, opts []gax.CallOption) []gax.CallOption {
+	if op.PathPrefix() == "" {
+		return opts
+	}
+
+	return append(opts, gax.WithPath(fmt.Sprintf("%s/%s/%s", op.PathPrefix(), version, op.Name())))
+}
---
+++ clients/v1/blockchain.go
@@ -0,0 +1,198 @@
+package v1
+
+import (
+	"context"
+
+	"github.com/coinbase/waas-client-library-go/clients"
+	blockchainpb "github.com/coinbase/waas-client-library-go/gen/go/coinbase/cloud/blockchain/v1"
+	innerClient "github.com/coinbase/waas-client-library-go/gen/go/coinbase/cloud/clients/v1"
+	"github.com/googleapis/gax-go/v2"
+	"google.golang.org/api/iterator"
+	"google.golang.org/grpc"
+)
+
+const (
+	// blockchainServiceName is the name of the BlockchainService used by the Authenticator.
+	blockchainServiceName = "waas_blockchain_service"
+
+	// blockchainServiceEndpoint is the default endpoint URL to use for BlockchainService.
+	blockchainServiceEndpoint = "https://api.developer.coinbase.com/waas/blockchain"
+)
+
+// BlockchainServiceClient is the client to use to access WaaS BlockchainService APIs.
+type BlockchainServiceClient struct {
+	client *innerClient.BlockchainClient
+}
+
+// NewBlockchainServiceClient returns a BlockchainServiceClient based on the given inputs.
+func NewBlockchainServiceClient(
+	ctx context.Context,
+	waasOpts ...clients.WaaSClientOption,
+) (*BlockchainServiceClient, error) {
+	config, err := clients.GetConfig(blockchainServiceName, blockchainServiceEndpoint, waasOpts...)
+	if err != nil {
+		return nil, err
+	}
+
+	clientOptions, err := clients.GetClientOptions(config)
+	if err != nil {
+		return nil, err
+	}
+
+	innerClient, err := innerClient.NewBlockchainRESTClient(ctx, clientOptions...)
+	if err != nil {
+		return nil, err
+	}
+
+	return &BlockchainServiceClient{
+		client: innerClient,
+	}, nil
+}
+
+// Close closes the connection to the API service. The user should invoke this when
+// the client is no longer required.
+func (p *BlockchainServiceClient) Close() error {
+	return p.client.Close()
+}
+
+// Connection returns a connection to the API service.
+//
+// Deprecated: Connections are now pooled so this method does not always
+// return the same resource.
+func (p *BlockchainServiceClient) Connection() *grpc.ClientConn {
+	return p.client.Connection()
+}
+
+// GetNetwork gets a Network.
+func (b *BlockchainServiceClient) GetNetwork(
+	ctx context.Context,
+	req *blockchainpb.GetNetworkRequest,
+	opts ...gax.CallOption) (*blockchainpb.Network, error) {
+	network, err := b.client.GetNetwork(ctx, req, opts...)
+
+	return network, clients.UnwrapError(err)
+}
+
+// NetworkIterator is an interface for iterating through the response to ListNetworks.
+type NetworkIterator interface {
+	// PageInfo supports pagination. See the google.golang.org/api/iterator package for details.
+	PageInfo() *iterator.PageInfo
+
+	// Next returns the next result. Its second return value is iterator.Done if there are no more
+	// results. Once Next returns Done, all subsequent calls will return Done.
+	Next() (*blockchainpb.Network, error)
+
+	// Response is the raw response for the current page.
+	// Calling Next() or InternalFetch() updates this value.
+	Response() *blockchainpb.ListNetworksResponse
+}
+
+// networkIteratorImpl is an implementation of NetworkIterator that unwraps correctly.
+type networkIteratorImpl struct {
+	iter *innerClient.NetworkIterator
+}
+
+// PageInfo supports pagination. See the google.golang.org/api/iterator package for details.
+func (n *networkIteratorImpl) PageInfo() *iterator.PageInfo {
+	return n.iter.PageInfo()
+}
+
+// Next returns the next result. Its second return value is iterator.Done if there are no more
+// results. Once Next returns Done, all subsequent calls will return Done.
+func (n *networkIteratorImpl) Next() (*blockchainpb.Network, error) {
+	network, err := n.iter.Next()
+
+	return network, clients.UnwrapError(err)
+}
+
+// Response is the raw response for the current page.
+// Calling Next() or InternalFetch() updates this value.
+func (n *networkIteratorImpl) Response() *blockchainpb.ListNetworksResponse {
+	if n.iter.Response == nil {
+		return nil
+	}
+
+	response := n.iter.Response.(*blockchainpb.ListNetworksResponse)
+
+	return response
+}
+
+// ListNetworks lists the Networks supported by WaaS APIs.
+func (b *BlockchainServiceClient) ListNetworks(
+	ctx context.Context,
+	req *blockchainpb.ListNetworksRequest,
+	opts ...gax.CallOption) NetworkIterator {
+	return &networkIteratorImpl{iter: b.client.ListNetworks(ctx, req, opts...)}
+}
+
+// GetAsset retrieves an Asset by resource name.
+func (b *BlockchainServiceClient) GetAsset(
+	ctx context.Context,
+	req *blockchainpb.GetAssetRequest,
+	opts ...gax.CallOption) (*blockchainpb.Asset, error) {
+	asset, err := b.client.GetAsset(ctx, req, opts...)
+
+	return asset, clients.UnwrapError(err)
+}
+
+// AssetIterator is an interface for iterating through the response to ListAssets.
+type AssetIterator interface {
+	// PageInfo supports pagination. See the google.golang.org/api/iterator package for details.
+	PageInfo() *iterator.PageInfo
+
+	// Next returns the next result. Its second return value is iterator.Done if there are no more
+	// results. Once Next returns Done, all subsequent calls will return Done.
+	Next() (*blockchainpb.Asset, error)
+
+	// Response is the raw response for the current page.
+	// Calling Next() or InternalFetch() updates this value.
+	Response() *blockchainpb.ListAssetsResponse
+}
+
+// assetIteratorImpl is an implementation of AssetIterator that unwraps correctly.
+type assetIteratorImpl struct {
+	iter *innerClient.AssetIterator
+}
+
+// PageInfo supports pagination. See the google.golang.org/api/iterator package for details.
+func (n *assetIteratorImpl) PageInfo() *iterator.PageInfo {
+	return n.iter.PageInfo()
+}
+
+// Next returns the next result. Its second return value is iterator.Done if there are no more
+// results. Once Next returns Done, all subsequent calls will return Done.
+func (n *assetIteratorImpl) Next() (*blockchainpb.Asset, error) {
+	asset, err := n.iter.Next()
+
+	return asset, clients.UnwrapError(err)
+}
+
+// Response is the raw response for the current page.
+// Calling Next() or InternalFetch() updates this value.
+func (n *assetIteratorImpl) Response() *blockchainpb.ListAssetsResponse {
+	if n.iter.Response == nil {
+		return nil
+	}
+
+	response := n.iter.Response.(*blockchainpb.ListAssetsResponse)
+
+	return response
+}
+
+// ListAssets lists the Assets supported by WaaS APIs.
+func (b *BlockchainServiceClient) ListAssets(
+	ctx context.Context,
+	req *blockchainpb.ListAssetsRequest,
+	opts ...gax.CallOption) AssetIterator {
+	return &assetIteratorImpl{b.client.ListAssets(ctx, req, opts...)}
+}
+
+// BatchGetAssets returns the list of Assets indicated by the given request.
+func (b *BlockchainServiceClient) BatchGetAssets(
+	ctx context.Context,
+	req *blockchainpb.BatchGetAssetsRequest,
+	opts ...gax.CallOption) (*blockchainpb.BatchGetAssetsResponse, error) {
+	asset, err := b.client.BatchGetAssets(ctx, req, opts...)
+
+	return asset, clients.UnwrapError(err)
+}
